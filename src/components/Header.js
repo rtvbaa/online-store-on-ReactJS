2c3f08b22ead9bc79796da0f463c43f53920b3d3
Andrey <asd@asd.com>
2022-06-07 19:11:24 +0300

    part 2.2

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,15 @@
-import React from 'react'
+    import React from 'react'
 
 export default function Header() {
   return (
     <header>
         <div>
             <span className='logo'>House Staff</span>
+            <ul className='nav'>
+            <li>Про нас</li>
+            <li>Контакты</li>
+            <li>Кабинет</li>
+                </ul>
         </div>
         <div className='presentation'></div>
     </header>
